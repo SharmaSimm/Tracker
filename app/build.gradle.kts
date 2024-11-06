0e11f6e29f21e1f1bc20dc5c536a9dcffefd3112
Shikshya Sharma <sharma.sim2022@gmail.com>
2024-11-06 18:33:47 -0500

    PTT repo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.myperiodtracker"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.myperiodtracker"
